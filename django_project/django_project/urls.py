393be959f94d18c4f089e5e8c82aefc7eacafb52
rohit singh <rohit2701singh@gmail.com>
2024-02-28 02:46:52 +0530

    lec-8 profile pic update and signals

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -26,9 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
     path('profile/', user_views.profile, name="profile"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name="login"),
     path('logout/', user_views.logout_view, name="logout"),
-    path('', include('blog.urls')),
+    path('', include('blog.urls')), 
 ]
 
 
