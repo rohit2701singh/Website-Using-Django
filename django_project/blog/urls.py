b5f42211b5acb86134ac37b614621218b230267d
rohit singh <rohit2701singh@gmail.com>
2024-03-04 00:47:57 +0530

    lec12 password reset using mail

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views     # from django_project.blog import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
+    path('user/<str:username>', UserPostListView.as_view(), name="user-posts"),
     path('about/', views.about, name="blog-about"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),   #<pk> primary key id
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete")   
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),   
 ]
 
 
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +137,10 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('SMTP_MAIL')
+EMAIL_HOST_PASSWORD = os.getenv('SMTP_MAIL_PASS')
